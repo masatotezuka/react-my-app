0f0971077b5be3b120e79c4e2afd8ee16e275f1e
masatotezuka <tez.0731.tez@gmail.com>
2022-01-13 23:44:54 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
+import PropTypes from "prop-types";
 
 const App = () => {
   const profiles = [
     { name: "taro", age: 10 },
     { name: "hanako", age: 23 },
-    { name: "yamada" },
+    { name: "yamada", age: 33 },
   ];
 
   return (
@@ -24,17 +25,9 @@ const User = (props) => {
   );
 };
 
-User.defaultProps = {
-  age: 1,
+User.propTypes = {
+  name: PropTypes.string,
+  age: PropTypes.number.isRequired,
 };
 
 export default App;
-
-var items = [1, 2, 3, 4, 5];
-
-//mapは配列を返す
-var result = items.map(function (value) {
-  return value * 2;
-});
-
-console.log(result);
