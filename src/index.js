f77afcac34a41320e9fc76c72ee1e22075378a78
masatotezuka <tez.0731.tez@gmail.com>
2022-01-19 22:51:11 +0900

    state-liftup

--- src/index.js
+++ src/index.js
@@ -2,38 +2,119 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 
-class NameForm extends React.Component {
+function BoilingVerdict(props) {
+  if (props.celsius >= 100) {
+    return <p>The water would boil</p>;
+  }
+  return <p>The water would not boil</p>;
+}
+
+class Caculator extends React.Component {
   constructor(props) {
     super(props);
-    this.state = { value: "" };
     this.handleChange = this.handleChange.bind(this);
-    this.handleSubmit = this.handleSubmit.bind(this);
+    this.state = { temperature: "" };
   }
 
-  handleChange(event) {
-    this.setState({ value: event.target.value });
+  handleChange(e) {
+    this.setState({ temperature: e.target.value });
   }
-
-  handleSubmit(event) {
-    alert("An essay was submitted: " + this.state.value);
-    event.preventDefault();
-  }
-
   render() {
+    const temperature = this.state.temperature;
     return (
-      <form onSubmit={this.handleSubmit}>
-        <label>
-          Name:
-          <input
-            type="text"
-            value={this.state.value}
-            onChange={this.handleChange}
-          />
-        </label>
-        <input type="submit" value="Submit" />
-      </form>
+      <fieldset>
+        <legend>Enter temperature in Celsius:</legend>
+        <input value={temperature} onChange={this.handleChange} />
+        <BoilingVerdict celsius={parseFloat(temperature)} />
+      </fieldset>
     );
   }
 }
 
-ReactDOM.render(<NameForm />, document.getElementById("root1"));
+function toCelsius(fahrenherit) {
+  return ((fahrenherit - 32) * 5) / 9;
+}
+
+function toFahrenheit(celsius) {
+  return (celsius * 9) / 5 + 32;
+}
+
+function tryConvert(temperature, convert) {
+  const input = parseFloat(temperature);
+  if (Number.isNaN(input)) {
+    return "";
+  }
+  const output = convert(input);
+  const rounded = Math.round(output * 1000) / 1000;
+  return rounded.toString();
+}
+
+const scaleNames = {
+  c: "Celsius",
+  f: "Fahrenheit",
+};
+
+class TemperatureInput extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleChange = this.handleChange.bind(this);
+  }
+  handleChange(e) {
+    this.props.onTempertureChange(e.target.value);
+  }
+  render() {
+    const temperature = this.props.temperature;
+    const scale = this.props.scale;
+    return (
+      <fieldset>
+        <legend>Enter temperature in {scaleNames[scale]}:</legend>
+        <input value={temperature} onChange={this.handleChange}></input>
+      </fieldset>
+    );
+  }
+}
+
+class Caculator2 extends React.Component {
+  constructor(props) {
+    super(props);
+    this.handleCelsiusChange = this.handleCelsiusChange.bind(this);
+    this.handleFahrenheitChange = this.handleFahrenheitChange.bind(this);
+    this.state = { temperature: "", scale: "c" };
+  }
+
+  handleCelsiusChange(temperature) {
+    this.setState({ scale: "c", temperature });
+  }
+
+  handleFahrenheitChange(temperature) {
+    this.setState({ scale: "f", temperature });
+  }
+
+  render() {
+    const scale = this.state.scale;
+    const temperature = this.state.temperature;
+    const celisius =
+      scale === "f" ? tryConvert(temperature, toCelsius) : temperature;
+    const fahrenherit =
+      scale === "c" ? tryConvert(temperature, toFahrenheit) : temperature;
+
+    return (
+      <React.Fragment>
+        <TemperatureInput
+          scale="c"
+          temperature={celisius}
+          onTempertureChange={this.handleCelsiusChange}
+        />
+        <TemperatureInput
+          scale="f"
+          temperature={fahrenherit}
+          onTempertureChange={this.handleFahrenheitChange}
+        />
+        <BoilingVerdict celisius={parseFloat(celisius)} />
+      </React.Fragment>
+    );
+  }
+}
+
+ReactDOM.render(<Caculator />, document.getElementById("root1"));
+ReactDOM.render(<Caculator2 />, document.getElementById("root2"));
